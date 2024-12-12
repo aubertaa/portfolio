fc62aef955953655b9140483be7fd196667f0666
aubertaa <aubert.aa@gmail.com>
2024-12-12 19:36:14 +0100

    logos

--- src/app/pages/competencies/competencies.component.ts
+++ src/app/pages/competencies/competencies.component.ts
@@ -1,18 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CompetenciesService } from '../../services/competencies.service';
-
 @Component({
   selector: 'app-competencies',
   templateUrl: './competencies.component.html',
   styleUrl: './competencies.component.scss'
 })
-export class CompetenciesComponent {
+export class CompetenciesComponent implements OnInit {
+  competencies: {
+    title: string;
+    category: string;
+    logo: string;
+    level: number;
+    description: string;
+    certification: string;
+  }[] = [];
 
 constructor(private competenciesService: CompetenciesService) { }
 
-getCompetencies() {
-  return this.competenciesService.getCompetencies();
- }
 
+  activeCompetency: any = null;
+
+  showDetails (competency: any): void {
+    this.activeCompetency = competency;
+  }
+
+  hideDetails (): void {
+    this.activeCompetency = null;
+  }
+
+  ngOnInit (): void {
+    this.competencies = this.competenciesService.getCompetencies();
+  }
 
 }
