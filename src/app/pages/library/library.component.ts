05be27d78810a2e8ba4cfa32631bc430a6c781de
aubertaa <aubert.aa@gmail.com>
2024-12-14 18:12:35 +0100

    books

--- src/app/pages/library/library.component.ts
+++ src/app/pages/library/library.component.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { LibraryService } from '../../services/library.service';
+
+export interface Book {
+  title: string;
+  author: string;
+  description: string;
+  category: string;
+  image: string;
+}
 
 @Component({
   selector: 'app-library',
   standalone: false,
-  
+
   templateUrl: './library.component.html',
-  styleUrl: './library.component.scss'
+  styleUrl: './library.component.scss',
 })
 export class LibraryComponent {
 
+  books: Book[] = [];
+  categories: string[] = [];
+
+  selectedBook: any = null;
+  showDetails: boolean = false;
+
+  constructor(private libraryService: LibraryService) {}
+
+  getBooks (category: string) {
+    return this.books.filter(book => book.category === category);
+  }
+
+  onBookClick(book: any) {
+    // Toggle description visibility
+    this.selectedBook = book;
+    this.showDetails = true;
+  }
+
+  ngOnInit (): void {
+    this.books = this.libraryService.getBooks();
+    this.categories = this.books.map(book => book.category)
+      .filter((category, index, categories) => categories.indexOf(category) === index);
+  }
+
+  closeModal() {
+    // Close the modal by resetting selectedCompetency
+    this.showDetails = false;
+    this.selectedBook = null;
+  }
+
+  // Close modal if clicked outside
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    const modalElement = document.querySelector('.popin-modal');
+    if (modalElement && !modalElement.contains(event.target as Node)) {
+      this.closeModal();
+    }
+  }
 }
--- src/app/services/library.service.ts
+++ src/app/services/library.service.ts
@@ -9,50 +9,69 @@ export class LibraryService {
       {
         title: 'Accelerate',
         author: 'Nicole Forsgren, Jez Humble, Gene Kim',
-        image: ''
+        description: 'The Science of Lean Software and DevOps: Building and Scaling High Performing Technology Organizations',
+        image: 'books/Accelerate.png',
+        category: 'Ingénierie logicielle'
       },
       {
         title: 'Dynamic Reteaming',
         author: 'Heidi Helfand',
-        image: ''
+        description: '',
+        image: 'books/Dynamic Reteaming.png',
+        category: 'Management / organisation'
       },
       {
         title: 'Team Topologies',
         author: 'Matthew Skelton, Manuel Pais, Ruth Malan',
-        image: ''
+        description: '',
+        image: 'books/Team Topologies.png',
+        category: 'Management / organisation'
       },
       {
         title: 'Scrum Guide',
         author: 'Ken Schwaber, Jeff Sutherland',
-        image: ''
+        description: '',
+        image: 'books/scrum_guide.png',
+        category: 'Ingénierie logicielle'
       },
       {
         title: 'Refactoring',
         author: 'Martin Fowler',
-        image: ''
+        description: '',
+        image: 'books/Refactoring.png',
+        category: 'Ingénierie logicielle'
       },
       {
         title: 'Measure what matters',
         author: 'John Doerr',
-        image: ''
+        description: '',
+        image: 'books/Measure What Matters.png',
+        category: 'Ingénierie logicielle'
       },
       {
         title: 'Management 3.0',
         author: 'Jurgen Appelo',
-        image: ''
+        description: '',
+        image: 'books/Management 3.0.png',
+        category: 'Management / organisation'
       },
       {
         title: 'L\'entreprise libérée',
         author: 'Isaac Getz',
-        image: ''
+        description: '',
+        image: 'books/L\'Entreprise libérée.png',
+        category: 'Management / organisation'
       },
       {
         title: 'How to change the world',
         author: 'Jurgen Appelo',
-        image: ''
+        description: '',
+        image: 'books/How to Change the World.png',
+        category: 'Management / organisation'
       }
     ]
   }
+
 
   constructor() { }
 }
