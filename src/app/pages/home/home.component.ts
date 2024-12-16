0a05150ac865be15d762d0579d88c0e2a0de6e6b
aubertaa <aubert.aa@gmail.com>
2024-12-16 20:09:05 +0100

    books / avis

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,7 +9,7 @@ export class HomeComponent {
 
   sections = [
     { title: 'Qui suis-je ?', description: 'En savoir plus à propos de moi et mon parcours', link: '/qui-suis-je' },
-    { title: 'Ma bibliothèque', description: 'Mes lectures professionnelles, ouvrages de référence et sources d\'inspiration', link: '/mes-competences' },
+    { title: 'Ma bibliothèque', description: 'Mes lectures professionnelles et sources d\'inspiration', link: '/mes-competences' },
     { title: 'Mes compétences', description: 'Découvrez mes compétences plus en détail', link: '/mes-competences' },
     { title: 'Mes projets', description: 'Découvrez mes projets et réalisations', link: '/projets' },
     { title: 'Mes accomplissements', description: 'Mes accomplissements, publications et interventions', link: '/projets' },
