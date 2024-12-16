f232498459793c4e6180a554d923ce069d6f8262
aubertaa <aubert.aa@gmail.com>
2024-12-16 20:44:50 +0100

    cosmetics

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -9,10 +9,10 @@ export class HomeComponent {
 
   sections = [
     { title: 'Qui suis-je ?', description: 'En savoir plus à propos de moi et mon parcours', link: '/qui-suis-je' },
-    { title: 'Ma bibliothèque', description: 'Mes lectures professionnelles et sources d\'inspiration', link: '/mes-competences' },
+    { title: 'Ma bibliothèque', description: 'Mes lectures professionnelles et sources d\'inspiration', link: '/library' },
     { title: 'Mes compétences', description: 'Découvrez mes compétences plus en détail', link: '/mes-competences' },
     { title: 'Mes projets', description: 'Découvrez mes projets et réalisations', link: '/projets' },
-    { title: 'Mes accomplissements', description: 'Mes accomplissements, publications et interventions', link: '/projets' },
+    { title: 'Mes accomplissements', description: 'Mes accomplissements, publications et interventions', link: '/achievements' },
     { title: 'Contact', description: 'Contactez-moi via le formulaire de contact', link: '/contact' },
   ];
 }
