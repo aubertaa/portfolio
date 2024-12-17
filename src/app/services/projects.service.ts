a38879f105bc25711db077adeca65d47a8d5226e
aubertaa <aubert.aa@gmail.com>
2024-12-17 08:15:41 +0100

    first projects descr

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -11,13 +11,15 @@ export class ProjectsService {
 
     return [
       {
-        title: 'Définition / implémentation d\'une politique et une stratégie de tests',
-        date: '2015',
+        title: 'Réorganisation d\'équipes en 3 pôles de développement',
+        date: '2025',
         domaine: 'Stratégie d\'entreprise',
         involved_competencies: [
           "",
           ""
         ],
+        image: '',
+        document: '',
         description: ''
       },
       {
@@ -25,39 +27,12 @@ export class ProjectsService {
         date: '2024',
         domaine: 'Stratégie d\'entreprise',
         involved_competencies: [
-          "",
-          ""
+          "PowerBI",
+          "Python",
+          "CI/CD"
         ],
-        description: ''
-      },
-      {
-        title: 'Déploiement de l\'intégration continue',
-        date: '2017 - 2018',
-        domaine: 'Stratégie d\'entreprise',
-        involved_competencies: [
-          "",
-          ""
-        ],
-        description: ''
-      },
-      {
-        title: 'Suivi / dashboarding des coûts cloud',
-        date: '2022',
-        domaine: 'Stratégie d\'entreprise',
-        involved_competencies: [
-          "",
-          ""
-        ],
-        description: ''
-      },
-      {
-        title: 'Réorganisation d\'équipes en 3 pôles de développement',
-        date: '2022 - 2025',
-        domaine: 'Stratégie d\'entreprise',
-        involved_competencies: [
-          "",
-          ""
-        ],
+        image: '',
+        document: '',
         description: ''
       },
       {
@@ -68,76 +43,9 @@ export class ProjectsService {
           "",
           ""
         ],
+        image: '',
+        document: '',
         description: ''
-      },
-      {
-        title: 'Automatisation de tests de non régression d\'interfaces',
-        date: '2018',
-        domaine: 'Tests logiciels',
-        involved_competencies: [
-          "",
-          ""
-        ],
-        description: ''
-      },
-      {
-        title: 'Automatisation de tests de non régression API de données analytics',
-        date: '2016',
-        domaine: 'Tests logiciels',
-        involved_competencies: [
-          "",
-          ""
-        ],
-        description: ''
-      },
-      {
-        title: 'Automatisation de tests de non régression client lourd / Add-In Excel',
-        date: '2014',
-        domaine: 'Tests logiciels',
-        involved_competencies: [
-          "",
-          ""
-        ],
-        description: ''
-      },
-      {
-        title: 'Automatisation de tests de non régression backend de stockage de données dans le cloud',
-        date: '2009',
-        domaine: 'Tests logiciels',
-        involved_competencies: [
-          "",
-          ""
-        ],
-        description: ''
-      },
-      {
-        title: 'HitMaker',
-        date: '2020',
-        domaine: 'Développement',
-        involved_competencies: [
-          "",
-          ""
-        ],
-        description: 'Génération de jeux de données de tests aléatoires, simulation de données réelles.'
-      },
-      {
-        title: 'MaCaveAMoi',
-        date: '2019',
-        domaine: 'Développement',
-        involved_competencies: [
-          "QuasarJS",
-          "Alexa Skills"
-        ],
-        description: 'Logiciel de gestion de cave en ligne : disponible sur le web, mobile (Android Play Store) et sur Alexa.'
-      },
-      {
-        title: 'VolatileEC2',
-        date: '2022',
-        domaine: 'Développement',
-        involved_competencies: [
-          "AWS"
-        ],
-        description: 'Environnement pédagogique en InfraAsCode utilisé pour la formation des alternants aux pratiques et outils de la CI et DevOps. Déploiement d\'une infrastructure de développement logiciel en un clic.'
       },
       {
         title: 'Backoffice utils',
@@ -149,7 +57,129 @@ export class ProjectsService {
           "Gitlab",
           "Sonarqube"
         ],
+        image: '',
+        document: '',
         description: 'Nombreux scripts de backoffice pour automatiser des tâches récurrentes: suppression de projets obsolètes (sonarqube), archivage automatique de repositories / contrôle des expirations de tokens (gitlab), alerting quotidien / hebdomadaire (respect des pratiques ISO27001, surveillance des environnements internes), extraction de données (JIRA, Gitlab, Sonarqube).'
+      },
+      {
+        title: 'Suivi / dashboarding des coûts cloud',
+        date: '2022',
+        domaine: 'Stratégie d\'entreprise',
+        involved_competencies: [
+          "",
+          ""
+        ],
+        image: '',
+        document: '',
+        description: ''
+      },
+      {
+        title: 'VolatileEC2',
+        date: '2022',
+        domaine: 'Développement',
+        involved_competencies: [
+          "AWS"
+        ],
+        image: '',
+        document: '',
+        description: 'Environnement pédagogique en InfraAsCode utilisé pour la formation des alternants aux pratiques et outils de la CI et DevOps. Déploiement d\'une infrastructure de développement logiciel en un clic.'
+      },
+      {
+        title: 'HitMaker',
+        date: '2020',
+        domaine: 'Développement',
+        involved_competencies: [
+          "",
+          ""
+        ],
+        image: '',
+        document: '',
+        description: 'Génération de jeux de données de tests aléatoires, simulation de données réelles.'
+      },
+      {
+        title: 'MaCaveAMoi',
+        date: '2019',
+        domaine: 'Développement',
+        involved_competencies: [
+          "QuasarJS",
+          "Alexa Skills"
+        ],
+        image: '',
+        document: '',
+        description: 'Logiciel de gestion de cave en ligne : disponible sur le web, mobile (Android Play Store) et sur Alexa.'
+      },
+      {
+        title: 'Automatisation de tests de non régression d\'interfaces',
+        date: '2018',
+        domaine: 'Tests logiciels',
+        involved_competencies: [
+          "",
+          ""
+        ],
+        image: '',
+        document: '',
+        description: ''
+      },
+      {
+        title: 'Déploiement de l\'intégration continue',
+        date: '2017',
+        domaine: 'Stratégie d\'entreprise',
+        involved_competencies: [
+          "CI/CD",
+          "DevOps",
+          "Management"
+        ],
+        image: '',
+        document: '',
+        description: ''
+      },
+      {
+        title: 'Automatisation de tests de non régression API de données analytics',
+        date: '2016',
+        domaine: 'Tests logiciels',
+        involved_competencies: [
+          "",
+          ""
+        ],
+        image: '',
+        document: '',
+        description: ''
+      },
+      {
+        title: 'Définition / implémentation d\'une politique et une stratégie de tests',
+        date: '2015',
+        domaine: 'Stratégie d\'entreprise',
+        involved_competencies: [
+          "Management de tests logiciels",
+          "Gestion de projet"
+        ],
+        image: '',
+        document: '',
+        description: 'Proposition, définition et mise en place d\'une politique de test d\'entreprise ou "Comment combiner pratiques de test et Agilité". Définition et implémentation progressive d\'une double stratégie de tests : anticipative pour éviter de nouvelles régressions en production et réactive pour gérer au mieux les régressions déjà présentes en production.'
+      },
+      {
+        title: 'Automatisation de tests de non régression client lourd / Add-In Excel',
+        date: '2014',
+        domaine: 'Tests logiciels',
+        involved_competencies: [
+          "",
+          ""
+        ],
+        image: '',
+        document: '',
+        description: ''
+      },
+      {
+        title: 'Automatisation de tests de non régression backend de stockage de données dans le cloud',
+        date: '2009',
+        domaine: 'Tests logiciels',
+        involved_competencies: [
+          "",
+          ""
+        ],
+        image: '',
+        document: '',
+        description: ''
       }
     ];
 
