3c9b8469f1c41282d6496a724e87b836563a67a5
aubertaa <aubert.aa@gmail.com>
2024-12-23 08:37:38 +0100

    adjustments

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PreloadImagesService } from '../../services/preload-images.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  constructor(
+    private preloadImagesService: PreloadImagesService
+  ) { }
 
   sections = [
     { title: 'Mes compétences', description: 'Découvrez mes compétences plus en détail', link: '/mes-competences' },
@@ -14,4 +18,13 @@ export class HomeComponent {
     { title: 'Ma bibliothèque', description: 'Mes lectures professionnelles et sources d\'inspiration', link: '/library' },
     { title: 'Contact', description: 'Contactez-moi via le formulaire de contact', link: '/contact' },
   ];
+
+
+  ngOnInit (): void {
+
+    // Preload image for the home page
+    this.preloadImagesService.preloadImages(["AUBERTAlexandre.jpg"]);
+
+  }
+
 }
